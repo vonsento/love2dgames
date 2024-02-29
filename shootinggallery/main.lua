ee1de0c1e93bb7154cf86376aa5a3d1d70bbb7d6
vonsento <156618267+vonsento@users.noreply.github.com>
2024-02-29 12:41:51 -0800

    shooting gallery update

--- shootinggallery/main.lua
+++ shootinggallery/main.lua
@@ -2,9 +2,12 @@ local target = {};
 -- data type: table
 -- this is the target that we will shoot
 
+local screen_width = love.graphics.getWidth();
+local screen_height = love.graphics.getHeight();
+
 target.radius = 50;
-target.x = math.random(target.radius, love.graphics.getWidth() - target.radius);
-target.y = math.random(target.radius, love.graphics.getHeight() - target.radius);
+target.x = love.math.random(target.radius, screen_width - target.radius);
+target.y = love.math.random(target.radius, screen_height - target.radius);
 
 local score = 0;
 -- data type: int
@@ -24,6 +27,14 @@ local sprites = {};
 -- data type: table
 -- this is a table containing all the sprites that we will use in the game
 
+local last_damage_time = 0.0;
+-- data type: double
+-- keep track of the last time received damage
+
+local game_time = 0.0;
+-- data type: double
+-- keep track of each second of the game
+
 sprites.sky = love.graphics.newImage('sprites/sky.png');
 sprites.target = love.graphics.newImage('sprites/target.png');
 sprites.crosshairs = love.graphics.newImage('sprites/crosshairs.png');
@@ -39,9 +50,11 @@ function love.update(dt)
         timer = 0;
         game_state = 1;
     end
+    game_time = game_time + dt;
 end
 
 function love.draw()
+    love.graphics.setColor(255, 255, 255);
     love.graphics.draw(sprites.sky, 0, 0);
     -- drawing the background of the game, its first so later other drawings are on top
 
@@ -56,14 +69,25 @@ function love.draw()
     love.graphics.print("Time: " .. game_timer, 300, 5);
 
     if game_state == 1 then
-        love.graphics.printf("Click to begin", 0, 250, love.graphics.getWidth(), "center");
+        love.graphics.printf("Click to begin", 0, 250, screen_width, "center");
     end
     
     if game_state == 2 then
         love.graphics.draw(sprites.target, target.x - target.radius, target.y - target.radius);
     end
 
-    love.graphics.draw(sprites.crosshairs, love.mouse.getX()-20, love.mouse.getY()-20);
+    local cursor_position_x = love.mouse.getX();
+    local cursor_position_y = love.mouse.getY();
+    love.graphics.draw(sprites.crosshairs, cursor_position_x -20, cursor_position_y -20);
+    
+    if game_time - last_damage_time < 0.33 and last_damage_time > 0.0 then
+        love.graphics.setColor(255, 0, 0, 0.24);
+        love.graphics.rectangle("fill", 0, 0, screen_width, screen_height);
+    end
+end
+
+local function get_2d_distance(x1, y1, x2, y2)
+    return ((x2 - x1) ^ 2 + (y2 - y1) ^ 2) ^ 0.5;
 end
 
 function love.mousepressed(x, y, button, istouch, presses)
@@ -81,17 +105,16 @@ function love.mousepressed(x, y, button, istouch, presses)
     end
 
     if game_state == 2 then
-        local distance = distanceBetween(x, y, target.x, target.y);
+        local distance = get_2d_distance(x, y, target.x, target.y);
         if distance <= target.radius then
             score = score + score_increment;
-            target.x = math.random(target.radius, love.graphics.getWidth() - target.radius);
-            target.y = math.random(target.radius, love.graphics.getHeight() - target.radius);
+            target.x = love.math.random(target.radius, screen_width - target.radius);
+            target.y = love.math.random(target.radius, screen_height - target.radius);
         else
-            score = score - score_increment;
+            if game_time - last_damage_time > 0.5 then
+                score = score - score_increment;
+                last_damage_time = game_time;
+            end
         end
     end
-end
- 
-function distanceBetween(x1, y1, x2, y2)
-    return math.sqrt((x2 - x1)^2 + (y2 - y1)^2)
 end
